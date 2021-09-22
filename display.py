d25ed216108c437be49cd3ad9f987492b910ce3c
Braden <bradenschulz@gmail.com>
2021-09-22 01:38:56 -0400

    first implemented aldousBroder, moved pictureGrid
    new code currently pretty messy -- uncommented & littered with debugging
    
    pictureGrid moved to a new file meant to house all ui and display stuff

---
+++ display.py
@@ -0,0 +1,46 @@
+# display/ui functions go here!
+
+''' keeping pictureGrid() here for future reference while attempting to get basic visuals working '''
+
+# """ saves input grid as a .jpg image in the folder "temp" in project root """
+# # currently half-finished because saving an image of the grid with the window hidden is not possible for pyGame
+# def pictureGrid(grid):
+
+#     pg.init()
+    
+#     BLACK = (0, 0, 0)
+#     WHITE = (255, 255, 255)
+#     BLUE =  (  0,   0, 255)
+#     GREEN = (  0, 255,   0)
+#     RED =   (255,   0,   0)
+    
+#     # initializes the screen with a grid-based resolution 
+#     screen = pg.display.set_mode(size=(len(grid)*50, len(grid)*50), flags = pg.HIDDEN)
+#     pg.display.iconify()
+    
+#     done = False
+#     clock = pg.time.Clock()
+
+#     # Clear the screen and set the screen background
+#     screen.fill(WHITE)
+
+#     while not done:
+#         pass
+#         # limits the loop to running 10 times per second, saving CPU
+#         clock.tick(10)
+        
+#         # lets the user close the window if they request to, ends loop
+#         for event in pg.event.get():
+#             if event.type == pg.QUIT:
+#                 done = True
+
+#         for i in range(len(grid)):
+#             for j in range(len(grid)):
+#                 if grid[i][j] == ' ':
+#                     pass
+#                 else:
+#                     pg.draw.line(screen, BLACK, [50*j, 24 + i*50], [50*(j+1), 24 + i*50], 50)
+                
+#         pg.display.flip()
+    
+#     pg.image.save(screen, "temp\grid_pic.jpg")
--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 import grid
 
-playerGrid = grid.makeGrid(2)
-print(playerGrid)
+playerGrid = grid.aldousBroder(14)
+
+
+''' debug code for viewing algorithm result '''
+import pygame as pg
+
+pg.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE =  (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED =   (255,   0,   0)
+
+# initializes the screen with a grid-based resolution 
+screen = pg.display.set_mode(size=(len(playerGrid)*50, len(playerGrid)*50))
+
+done = False
+clock = pg.time.Clock()
+# Clear the screen and set the screen background
+screen.fill(WHITE)
+while not done:
+    # limits the loop to running 10 times per second, saving CPU
+    clock.tick(10)
+    
+    # lets the user close the window if they request to, ends loop
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            done = True
+    for i in range(len(playerGrid)):
+        for j in range(len(playerGrid)):
+            if playerGrid[i, j] == 1 or playerGrid [i,j] == 2:
+                pass
+            else:
+                pg.draw.line(screen, BLACK, [50*j, 24 + i*50], [50*(j+1), 24 + i*50], 50)
+            
+    pg.display.flip()
