4542efe91d004044aa4916e903d2d3798f86b0a9
Braden <bradenschulz@gmail.com>
2021-10-17 17:03:20 -0400

    finished implementing necessary drawing objects exits look fine, so now we'll start putting drawing to practical use

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from OpenGL import *
 from OpenGL.GL import *
 from OpenGL.arrays import vbo
 
-''' startup routine '''
-# playerGrid = grid.aldousBroder(2)
-
+''' OpenGL setup '''
 # checks user's OpenGL version (only creates window if sufficient)
 glfw.init()
 glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
@@ -37,25 +35,29 @@ glBindVertexArray(wallVAO)
 # initializes vertex data corresponding to all vertices necessary for drawing walls and openings
 # (vertices laid out as "wall corners" -- four corners are drawn by default, so we draw walls by connecting them)
 wallVertices = np.array( [-0.8,  0.8, 0.0,  # 0 - top left corner; top left
-                      -0.6,  0.8, 0.0,  # 1 - top left corner; top right
-                      -0.8,  0.6, 0.0,  # 2 - top left corner; bottom left
-                      -0.6,  0.6, 0.0,  # 3 - top left corner; bottom right
+                          -0.6,  0.8, 0.0,  # 1 - top left corner; top right
+                          -0.8,  0.6, 0.0,  # 2 - top left corner; bottom left
+                          -0.6,  0.6, 0.0,  # 3 - top left corner; bottom right
+  
+                           0.6,  0.8, 0.0,  # 4 - top right corner; top left
+                           0.8,  0.8, 0.0,  # 5 - top right corner; top right
+                           0.6,  0.6, 0.0,  # 6 - top right corner; bottom left
+                           0.8,  0.6, 0.0,  # 7 - top right corner; bottom right
+   
+                          -0.8, -0.6, 0.0,  # 8 - bottom left corner; top left
+                          -0.6, -0.6, 0.0,  # 9 - bottom left corner; top right
+                          -0.8, -0.8, 0.0,  # 10 - bottom left corner; bottom left
+                          -0.6, -0.8, 0.0,  # 11 - bottom left corner; bottom right
+                           
+                           0.6, -0.6, 0.0,  # 12 - bottom right corner; top left
+                           0.8, -0.6, 0.0,  # 13 - bottom right corner; top right
+                           0.6, -0.8, 0.0,  # 14 - bottom right corner; bottom left
+                           0.8, -0.8, 0.0]  # 15 - bottom right corner; bottom right
+                          , dtype = 'float32')
 
-                       0.6,  0.8, 0.0,  # 4 - top right corner; top left
-                       0.8,  0.8, 0.0,  # 5 - top right corner; top right
-                       0.6,  0.6, 0.0,  # 6 - top right corner; bottom left
-                       0.8,  0.6, 0.0,  # 7 - top right corner; bottom right
-
-                      -0.8, -0.6, 0.0,  # 8 - bottom left corner; top left
-                      -0.6, -0.6, 0.0,  # 9 - bottom left corner; top right
-                      -0.8, -0.8, 0.0,  # 10 - bottom left corner; bottom left
-                      -0.6, -0.8, 0.0,  # 11 - bottom left corner; bottom right
-                       
-                       0.6, -0.6, 0.0,  # 12 - bottom right corner; top left
-                       0.8, -0.6, 0.0,  # 13 - bottom right corner; top right
-                       0.6, -0.8, 0.0,  # 14 - bottom right corner; bottom left
-                       0.8, -0.8, 0.0]  # 15 - bottom right corner; bottom right
-                      , dtype = 'float32')
+# creates and binds vertex buffer object to contain wall vertex data
+wallVBO = vbo.VBO(wallVertices)
+wallVBO.bind()
 
 # defines triangle indices for drawing walls from vertex data and binds them to corresponding element buffer objects
 cornerIndices = np.array( [0, 1, 2,     # top left corner
@@ -72,30 +74,71 @@ cornerEBO = vbo.VBO(cornerIndices, target=GL_ELEMENT_ARRAY_BUFFER)
 leftWallIndices = np.array( [2, 3, 8,       
                              8, 9, 3]
                             , dtype = 'uint32')
-leftEBO = vbo.VBO(leftWallIndices, target=GL_ELEMENT_ARRAY_BUFFER)
+leftWallEBO = vbo.VBO(leftWallIndices, target=GL_ELEMENT_ARRAY_BUFFER)
 
 bottomWallIndices = np.array( [11, 9, 12,
                                12, 14, 11]
                               , dtype = 'uint32')
-bottomEBO = vbo.VBO(bottomWallIndices, target=GL_ELEMENT_ARRAY_BUFFER)
+bottomWallEBO = vbo.VBO(bottomWallIndices, target=GL_ELEMENT_ARRAY_BUFFER)
 
 topWallIndices = np.array( [1, 3, 6,
                             6, 4, 1]
                             , dtype = 'uint32')
-topEBO = vbo.VBO(topWallIndices, target=GL_ELEMENT_ARRAY_BUFFER)
+topWallEBO = vbo.VBO(topWallIndices, target=GL_ELEMENT_ARRAY_BUFFER)
 
 rightWallIndices = np.array( [6, 7, 12,
                               12, 13, 7]
                               , dtype = 'uint32')
-rightEBO = vbo.VBO(rightWallIndices, target=GL_ELEMENT_ARRAY_BUFFER)
+rightWallEBO = vbo.VBO(rightWallIndices, target=GL_ELEMENT_ARRAY_BUFFER)
 
-# creates and binds vertex buffer object to contain wall vertex data
-wallVBO = vbo.VBO(wallVertices)
-wallVBO.bind()
-
-# denotes how vertex data should be read from the buffer and gives the go-ahead to do this
+# denotes how wall vertex data should be read from the buffer and gives the go-ahead to do this
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
 glEnableVertexAttribArray(0)
+
+# unbinds wall VAO and VBO so we can define equivalent objects for exits
+glBindVertexArray(0)
+wallVBO.bind()
+
+# creates and binds vertex array object for exits
+exitVAO = GLuint(0)
+glGenVertexArrays(1, exitVAO)
+glBindVertexArray(exitVAO)
+
+# initializes vertex data corresponding to all vertices necessary for drawing exits
+exitVertices = np.array( [-0.9,  1.0, 0.0,  # 0-2 - top exit
+                           0.9,  1.0, 0.0,  
+                           0.0,  0.4, 0.0,   
+                          
+                           1.0,  0.9, 0.0,   # 3-5 - right exit
+                           1.0, -0.9, 0.0,  
+                           0.4,  0.0, 0.0,
+                          
+                          -0.9, -1.0, 0.0,  # 6-8 - bottom exit
+                           0.9, -1.0, 0.0,
+                           0.0, -0.4, 0.0,
+                           
+                          -1.0,  0.9, 0.0,  # 9-11 - left exit
+                          -1.0, -0.9, 0.0,
+                          -0.4,  0.0, 0.0]
+                          , dtype = 'float32')
+
+# creates and binds vertex buffer object to contain exit vertex data
+exitVBO = vbo.VBO(exitVertices)
+exitVBO.bind()
+
+# binds triangle indices for drawing exits from vertex data to corresponding element buffer objects
+topExitEBO = vbo.VBO(np.array([0, 1, 2], dtype = 'uint32'), target=GL_ELEMENT_ARRAY_BUFFER)
+rightExitEBO = vbo.VBO(np.array([3, 4, 5], dtype = 'uint32'), target=GL_ELEMENT_ARRAY_BUFFER)
+bottomExitEBO = vbo.VBO(np.array([6, 7, 8], dtype = 'uint32'), target=GL_ELEMENT_ARRAY_BUFFER)
+leftExitEBO = vbo.VBO(np.array([9, 10, 11], dtype = 'uint32'), target=GL_ELEMENT_ARRAY_BUFFER)
+
+# denotes how exit vertex data should be read from the buffer and gives the go-ahead to do this
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+glEnableVertexAttribArray(0)
+
+# unbinds exit VAO and VBO to clean state for drawing
+glBindVertexArray(0)
+exitVBO.bind()
 
 # defines and binds vertex shader source to shader object, then compiles it
 vertexShaderSource = '''
@@ -111,9 +154,9 @@ glShaderSource(vertexShader, vertexShaderSource)
 glCompileShader(vertexShader)
 
 if(not glGetShaderiv(vertexShader, GL_COMPILE_STATUS)):
-    print("Vertex shader compilation failed:", glGetShaderInfoLog(vertexShader))
+    print("ERROR: Vertex shader compilation failed:", glGetShaderInfoLog(vertexShader))
 
-# defines and binds fragment shader source code to the shader object, then compiles it
+# defines and binds each fragment shader's source code to shader objects and compiles them
 wallFragmentShaderSource = '''
 #version 330 core
 out vec4 FragColor;
@@ -122,20 +165,46 @@ void main()
     FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 }
 '''
-fragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
-glShaderSource(fragmentShader, wallFragmentShaderSource)
-glCompileShader(fragmentShader)
+wallFragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
+glShaderSource(wallFragmentShader, wallFragmentShaderSource)
+glCompileShader(wallFragmentShader)
 
-if(not glGetShaderiv(fragmentShader, GL_COMPILE_STATUS)):
-    print("Fragment shader compilation failed:", glGetShaderInfoLog(fragmentShader))
+if(not glGetShaderiv(wallFragmentShader, GL_COMPILE_STATUS)):
+    print("ERROR: Wall fragment shader compilation failed:", glGetShaderInfoLog(wallFragmentShader))
 
-# initializes and links shader program employing defined vertex and fragment shaders
-shaderProgram = glCreateProgram()
-glAttachShader(shaderProgram, vertexShader)
-glAttachShader(shaderProgram, fragmentShader)
-glLinkProgram(shaderProgram)
+exitFragmentShaderSource = '''
+#version 330 core
+out vec4 FragColor;
+void main()
+{
+    FragColor = vec4(1.0f, 1.0f, 0.75f, 1.0f);
+}
+'''
+exitFragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
+glShaderSource(exitFragmentShader, exitFragmentShaderSource)
+glCompileShader(exitFragmentShader)
+
+if(not glGetShaderiv(exitFragmentShader, GL_COMPILE_STATUS)):
+    print("ERROR: Exit fragment shader compilation failed:", glGetShaderInfoLog(exitFragmentShader))
+
+# initializes and links shader programs for walls and exits
+wallShader = glCreateProgram()
+glAttachShader(wallShader, vertexShader)
+glAttachShader(wallShader, wallFragmentShader)
+glLinkProgram(wallShader)
+
+exitShader = glCreateProgram()
+glAttachShader(exitShader, vertexShader)
+glAttachShader(exitShader, exitFragmentShader)
+glLinkProgram(exitShader)
+
 glDeleteShader(vertexShader)
-glDeleteShader(fragmentShader)
+glDeleteShader(wallFragmentShader)
+glDeleteShader(exitFragmentShader)
+
+
+''' game setup '''
+playerGrid = grid.aldousBroder(2)
 
 
 ''' main loop '''
@@ -144,21 +213,30 @@ while(not glfw.window_should_close(window)):
 
     # clear screen to black
     glClear(GL_COLOR_BUFFER_BIT)
-    glUseProgram(shaderProgram)
 
+    # sets conditions for drawing walls
+    glUseProgram(wallShader)
+    glBindVertexArray(wallVAO)
     # draws an L (and extraneous corner), befitting of our current state
     cornerEBO.bind()
     glDrawElements(GL_TRIANGLES, 3*8, GL_UNSIGNED_INT, None)
-    bottomEBO.bind()
+    bottomWallEBO.bind()
     glDrawElements(GL_TRIANGLES, 3*2, GL_UNSIGNED_INT, None)
-    leftEBO.bind()
+    leftWallEBO.bind()
     glDrawElements(GL_TRIANGLES, 3*2, GL_UNSIGNED_INT, None)
+
+    # sets conditions for drawing exits
+    glUseProgram(exitShader)
+    glBindVertexArray(exitVAO)
+    # draws an exit to the right
+    rightExitEBO.bind()
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, None)
 
     # swap colour buffers to update display & address any events
     glfw.swap_buffers(window)
     glfw.poll_events()
 
-# function to be used to draw current position to the screen -- it needs a *little* work...
+# draws the player's surroundings in their current position to the screen -- it needs a *little* work...
 def renderPosition():
     pass
 
